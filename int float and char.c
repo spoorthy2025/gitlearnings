41ba94cc15be9529d24f716e302be870dc01f544
spoorthy2025 <spoorthyk23@gmail.com>
2025-11-10 06:29:56 -0800

    Add files via upload

---
+++ int float and char.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+  int a=45;
+  float b=12.9876;
+  char c='z';
+  printf("integer:%d\n",a);
+   printf("float(2 decimals):%.2f\n",b);
+  printf("float(5 decimals):%.5f\n",b);
+  printf("character:%c\n",c);
+ }
