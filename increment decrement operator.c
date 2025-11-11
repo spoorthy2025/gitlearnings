705625791843cabf291e1548bd1cd8333ed68a9b
spoorthy2025 <spoorthyk23@gmail.com>
2025-11-10 19:06:30 -0800

    Add files via upload

---
+++ increment decrement operator.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+  int i=5;
+   printf("i++=%d\n",i++);//post-increment
+   printf("++i=%d\n",++i);//pre-increment
+   printf("i--=%d\n",i--);//post-increment
+    printf("--i=%d\n",--i);//pre-increment
+    return 0;
+
+}
