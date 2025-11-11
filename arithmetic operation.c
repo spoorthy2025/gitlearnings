538dced525aea640c2f998101ca5f7119eaf479f
spoorthy2025 <spoorthyk23@gmail.com>
2025-11-10 18:42:55 -0800

    Add files via upload

---
+++ arithmetic operation.c
@@ -0,0 +1,10 @@
+# include<stdio.h>
+int main(){
+  int a=10,b=4;
+  printf("addition=%d\n",a+b);
+  printf("subtraction=%d\n",a-b);
+  printf("multiplication=%d\n",a*b);
+  printf("division=%d\n",a/b);
+   printf("modulus=%d\n",a%b);
+   return 0;
+}
