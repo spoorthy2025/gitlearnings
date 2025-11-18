70ba0336c7b2eaa9f3d06a533cb79cded7c5cfd8
spoorthy2025 <spoorthyk23@gmail.com>
2025-11-18 01:30:22 -0800

    Add files via upload

---
+++ do while.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+
+int main() {
+    double number, sum = 0;
+
+
+    do {
+        printf("Enter a number (0 to exit): ");
+        scanf("%lf", &number);
+        sum += number;
+    } while (number != 0.0);
+
+    printf("Sum = %.2lf\n", sum);
+
+    return 0;
+}
