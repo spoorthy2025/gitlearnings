4d8db1f72472c624b160fac8f1cb6ce52f2b9c08
spoorthy2025 <spoorthyk23@gmail.com>
2025-11-18 01:35:40 -0800

    Add files via upload

---
+++ msprogram2.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int main()
+{
+  float num1,num2,result;
+  char op;
+  printf("enter an expression (example:5+3):");
+  scanf("%f%c%f",num1,op ,num2);
+  switch(op)
+{
+  case'+':
+      result=num1+num2;
+      printf("result=%.2f\n",result);
+      break;
+  case'/':
+    if(num2!=0)
+        result=num1/num2;
+    else{
+         printf("error!division by zero.\n");
+        return 0;
+    }
+    printf("result=%.2f\n",result);
+    break;
+  default:
+    printf("invalid operart!\n");
+
+}
+  return 0;
+}
