737414dd0bb0ec09a85c7e9a703980d6f0caa258
spoorthy2025 <spoorthyk23@gmail.com>
2025-11-21 06:38:23 -0800

    Add files via upload

---
+++ different data types.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+  int a=10;
+  float b=3.24;
+  char c='z';
+  double d=12.3456789;
+  printf("integer:%d\n",a);
+   printf("float:%.2f\n",b);
+  printf("character:%c\n",c);
+  printf("double:%d\n",c);
+ }
